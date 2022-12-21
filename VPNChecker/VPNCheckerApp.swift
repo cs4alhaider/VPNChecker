f5fc0a51d364168871924e4936edec2383d6d67a
Abdullah Alhaider <cs.alhaider@gmail.com>
2022-12-21 12:07:46 +0300

    Initial Commit

---
+++ VPNChecker/VPNCheckerApp.swift
@@ -0,0 +1,17 @@
+//
+//  VPNCheckerApp.swift
+//  VPNChecker
+//
+//  Created by 16Root19 on 21/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct VPNCheckerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
